a5eb666a1a962c817a777b2fd766bf44b11653d3
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-03-11 08:12:46 +0900

    完成

--- Calendar/ViewController.swift
+++ Calendar/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
